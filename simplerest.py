53521928c685490e8d63d90f28dc37b776a45140
Stephen Turner <sturner@mit.edu>
2014-11-23 19:33:18 -0500

    Context root is now under /apis EMP api now under apis/emps Redir under apis/redir (reflects back HTTP params)

--- simplerest.py
+++ simplerest.py
@@ -4,27 +4,57 @@ from twisted.web.resource import Resource
 from twisted.internet import reactor
 import time
 import os
-
+import json
 import cgi
 
 class FormPage(Resource):
+    isLeaf = False
+
     def __init__(self):
         return
+
+    def getChild(self, name, request):
+        if name == '':
+            return self
+        return Resource.getChild(self, name, request)
 
     def render_GET(self, request):
         request.setHeader("Content-Type", "application/json")
         emp=request.args['emp'][0]
-        print emp
+
+        print request.path
+
         resString = ""
+        #if request.path == "/apis/redir":
+            #resString = "{ \"args\":" + json.dumps(str(request.args)) + "}"
+        #elif emp == "1":
         if emp == "1":
-            resString = "{ \"employees\": [ { \"firstName\":\"John\" , \"lastName\":\"Doe\" } ] }"
+            resString = "{ \"employees\": [ { \"firstName\":\"John\" , \"lastName\":\"Doe\" } ], \"args\":" + json.dumps(str(request.args)) + "}"
         else :
             resString = "{ \"employees\": [ { \"firstName\":\"Anna\" , \"lastName\":\"Smith\" } ] }"
         
         return resString
 
+class RedirectPoint(Resource):
+    def __init__(self):
+        return
+
+    def render_GET(self, request):
+        request.setHeader("Content-Type", "application/json")
+
+        print request.path
+
+        resString = ""
+        resString = "{ \"args\":" + json.dumps(str(request.args)) + "}"
+        
+        return resString
+
 root = Resource()
-root.putChild("apis", FormPage())
+context = Resource()
+formPage = FormPage()
+root.putChild("apis", context)
+context.putChild("redir", RedirectPoint())
+context.putChild("emps", formPage)
 factory = Site(root)
 port = os.environ.get("PORT", "8880")
 reactor.listenTCP(int(port), factory)
